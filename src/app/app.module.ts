5e73df4fda09fce74a2bfb9458248750a455b632
chandrukumar <chandru.dec26@gmail.com>
2023-08-13 20:32:57 +0530

    T-Hub project base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { HomeComponent } from './home/home.component';
+import { AccordianComponent } from './accordian/accordian.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    HomeComponent,
+    AccordianComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
